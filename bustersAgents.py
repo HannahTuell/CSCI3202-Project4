5a741ff11fc6ef7fd71632792d7fced984ab375f
Hannah Tuell <hannah.tuell@colorado.edu>
2013-12-01 16:27:51 -0700

    question 3 done

--- bustersAgents.py
+++ bustersAgents.py
@@ -1,16 +1,27 @@
 # bustersAgents.py
 # ----------------
 # Licensing Information:  You are free to use or extend these projects for 
+
 # educational purposes provided that (1) you do not distribute or publish 
+
 # solutions, (2) you retain this notice, and (3) you provide clear 
+
 # attribution to UC Berkeley, including a link to 
+
 # http://inst.eecs.berkeley.edu/~cs188/pacman/pacman.html
+
 # 
+
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+
 # The core projects and autograders were primarily created by John DeNero 
+
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+
 # Student side autograding was added by Brad Miller, Nick Hay, and 
+
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
 
 
 import util
@@ -157,4 +168,33 @@ class GreedyBustersAgent(BustersAgent):
                                             in enumerate(self.ghostBeliefs)
                                             if livingGhosts[i+1]]
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # initialize the closest ghost information for comparisons
+        closestGhostDist = float("inf")
+        closestPosition = None
+        # for each living ghost distribution
+        for ghostDistributions in livingGhostPositionDistributions:
+            prob = 0
+            position = None
+            # go through each finding the most likely position, with its probability
+            for p in ghostDistributions:
+                if ghostDistributions[p] > prob:
+                    prob = ghostDistributions[p]
+                    position = p
+            # then find the maze distance there and store it if it is the closest
+            # found so far
+            distance = self.distancer.getDistance(pacmanPosition, position)
+            if distance < closestGhostDist:
+                closestGhostDist = distance
+                closestPosition = position
+        # for each possible action, calculate the successor position and return
+        # the action that results in the shortest distance
+        bestAction = None
+        shortestDistance = closestGhostDist
+        for action in legal:
+            successorPosition = Actions.getSuccessor(pacmanPosition, action)
+            if self.distancer.getDistance(successorPosition, closestPosition) < shortestDistance:
+                shortestDistance = self.distancer.getDistance(successorPosition, closestPosition)
+                bestAction = action
+        return bestAction
+                                                
+        "*** END YOUR CODE HERE ***"
